0dc8ca629ed7e3308cf321b82f6d64cc481f13c9
Paulo <paulovsp16@hotmail.com>
2021-04-08 08:42:54 -0300

    teste tema pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,29 +4,55 @@
 AUTHOR = 'Paulo Vitor'
 SITENAME = 'Python and Beer'
 SITEURL = ''
-
+THEME = "pelican-themes/pelican-blue/"
 PATH = 'content'
 
 TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# DEFAULT #
+# # Feed generation is usually not desired when developing
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
+
+# # Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/itpaulovitor/'),
+          ('github', 'https://github.com/Splatter-dev'),
+          ('twitter', 'https://twitter.com/paulovsp16'),
+          )
+
+SIDEBAR_DIGEST = 'Programmer and Web Developer'
+
+FAVICON = 'url-to-favicon'
+
+DISPLAY_PAGES_ON_MENU = True
+
+TWITTER_USERNAME = '@paulovsp16'
+
+MENUITEMS = (('Blog', 'https://splatter-dev.github.io/'),)
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DISQUS_SITENAME = ""
+GOOGLE_ANALYTICS = ""
+
 
 DEFAULT_PAGINATION = 10
 
